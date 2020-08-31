3a9e04a64ff0eec2f15b3338509192969d0751f2
Maurya <Aruna.Maurya@philips.com>
2020-08-31 22:23:19 +0530

    Added code.

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,11 @@
 #include "stats.h"
+using namespace Statistics;
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
+Stats Statistics::ComputeStatistics(const std::vector<float>& list ) {
     //Implement statistics here
+    Stats statistics;
+    statistics.ComputeAverage(list);
+    statistics.ComputeMax(list);
+    statistics.ComputeMin(list);
+    return statistics;
 }
--- stats.h
+++ stats.h
@@ -1,5 +1,29 @@
 #include <vector>
+#include <algorithm>
 
 namespace Statistics {
-    Stats ComputeStatistics(const std::vector<___>& );
+    class Stats
+    {
+        public: 
+            float average;
+            float max;
+            float min;
+            void ComputeAverage(const std::vector<float>& list)
+            {
+                float sum = 0.0;
+                for(int i=0;i<list.size();i++)
+                sum += list[i];
+
+                average = sum/list.size();
+            }
+            void ComputeMin( const std::vector<float>& list)
+            {
+               min = *std::min_element(list.begin(), list.end());
+            }
+            void ComputeMax(const std::vector<float>& list)
+            {
+                max = *std::max_element(list.begin(), list.end());
+            }
+    };
+    Stats ComputeStatistics(const std::vector<float>& list);
 }
