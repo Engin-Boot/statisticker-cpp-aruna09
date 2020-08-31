8d2a117237f9933dfa913a1c8e506319947e2fd1
Maurya <Aruna.Maurya@philips.com>
2020-08-31 22:45:23 +0530

    Added test cases

--- stats-test.cpp
+++ stats-test.cpp
@@ -15,6 +15,9 @@ TEST_CASE("reports average, minimum and maximum") {
 
 TEST_CASE("average is NaN for empty array") {
     auto computedStats = Statistics::ComputeStatistics({});
+    isnan(computedStats.average);
+    isnan(computedStats.max);
+    isnan(computedStats.min);
     //All fields of computedStats (average, max, min) must be
     //NAN (not-a-number), as defined in math.h
     
